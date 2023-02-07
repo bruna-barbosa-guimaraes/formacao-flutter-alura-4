e63f127a7cf51904eb2d0baaaac449f8da03b41b
Bruna Guimarães <bruna.guimaraes@qyon.com>
2023-02-07 11:26:56 -0300

    mocks and stubs

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -1,9 +1,9 @@
-import 'package:estilizacao_componentes/data/bank_http.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatefulWidget {
-  const Header({Key? key}) : super(key: key);
+  const Header({required this.api, Key? key}) : super(key: key);
+  final Future<String> api;
 
   @override
   State<Header> createState() => _HeaderState();
@@ -59,7 +59,7 @@ class _HeaderState extends State<Header> {
                     ],
                   ),
                   FutureBuilder(
-                      future: BankHttp().dolarToReal(),
+                      future: widget.api,
                       builder: (context, snapshot) {
                         switch (snapshot.connectionState) {
                           case ConnectionState.none:
